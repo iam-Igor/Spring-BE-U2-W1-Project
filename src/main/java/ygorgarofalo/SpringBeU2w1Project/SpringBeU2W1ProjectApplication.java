55da3a080a525db976c6b440d6fe1b6c0b677bca
Ygor Garofalo <igorgarofalo@gmail.com>
2024-01-12 09:23:10 +0100

    First commit

---
+++ src/main/java/ygorgarofalo/SpringBeU2w1Project/SpringBeU2W1ProjectApplication.java
@@ -0,0 +1,13 @@
+package ygorgarofalo.SpringBeU2w1Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeU2W1ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBeU2W1ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ygorgarofalo/SpringBeU2w1Project/SpringBeU2W1ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ygorgarofalo.SpringBeU2w1Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBeU2W1ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
